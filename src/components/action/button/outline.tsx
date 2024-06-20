205575a477e64da60b9b306ccca77397c5430c7c
Maiane Gabriele (@sheisacreative) <creative@maiane.com.br>
2024-06-20 14:54:30 -0300

    Added navigation bar

--- src/components/action/button/outline.tsx
+++ src/components/action/button/outline.tsx
@@ -4,11 +4,25 @@ import styles from "./outline.module.css";
 type ButtonBlackProps = React.ComponentProps<"a"> & {
   text: string;
   href: string;
+  hasBottomBorder?: boolean;
+  hasTopBorder?: boolean;
 };
 
-export default async function ButtonOutline({ text, href }: ButtonBlackProps) {
+export default async function ButtonOutline({
+  text,
+  href,
+  hasBottomBorder = true,
+  hasTopBorder = true,
+}: ButtonBlackProps) {
   return (
-    <Link href={href} className={styles.button}>
+    <Link
+      href={href}
+      className={styles.button}
+      style={{
+        borderBottom: !hasBottomBorder ? "none" : "",
+        borderTop: !hasTopBorder ? "none" : "",
+      }}
+    >
       {text}
     </Link>
   );
--- src/components/navigation/bar/navigation-bar.tsx
+++ src/components/navigation/bar/navigation-bar.tsx
@@ -1,14 +1,51 @@
+import styles from "./navigation-bar.module.css";
 import ButtonOutline from "@/components/action/button/outline";
 import ButtonPrimaryBlack from "@/components/action/button/primary-black";
-import ButtonPrimaryPink from "@/components/action/button/primary-pink";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function NavigationBar() {
   return (
-    <header>
-      <h1>Bar</h1>
-      <ButtonPrimaryBlack text="Start Selling" href="#" />
-      <ButtonPrimaryPink text="Start Selling" href="#" />
-      <ButtonOutline text="Start Selling" href="#" />
+    <header className={styles.header}>
+      {/* Brand */}
+      <Image
+        src={"/brand/gumroad.svg"}
+        width={300}
+        height={40}
+        alt="Gumroad"
+        className={styles.brand}
+      />
+
+      {/* Navigation */}
+      <nav>
+        <ul className={styles.navigationList}>
+          <li>
+            <Link href={"#"}>Features</Link>
+          </li>
+          <li>
+            <Link href={"#"}>Pricing</Link>
+          </li>
+          <li>
+            <Link href={"#"}>Blog</Link>
+          </li>
+          <li>
+            <Link href={"#"} className={styles.active}>
+              Discover
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Buttons */}
+      <div className={styles.buttonGroup}>
+        <ButtonOutline
+          text="Start Selling"
+          href="#"
+          hasBottomBorder={false}
+          hasTopBorder={false}
+        />
+        <ButtonPrimaryBlack text="Start Selling" href="#" />
+      </div>
     </header>
   );
 }
