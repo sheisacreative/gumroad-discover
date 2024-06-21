405f82dfcb5b15e501adbe069841a563f19009c4
Maiane Gabriele (@sheisacreative) <creative@maiane.com.br>
2024-06-21 14:00:59 -0300

    Desktop version ok

--- src/sections/staff-picks.tsx
+++ src/sections/staff-picks.tsx
@@ -1,7 +1,28 @@
+import ProductCardList from "@/components/action/card/product-card-list";
+import { staffPicks } from "@/content/staff-picks";
+import Link from "next/link";
+import styles from "./staff-picks.module.css";
+
 export default async function StaffPicks() {
   return (
-    <section>
-      <h1>Sections</h1>
+    <section className={styles.staffSection}>
+      {/* Title */}
+      <div className={styles.titleContainer}>
+        <h2 className={`display-medium ${styles.title}`}>
+          From our heart, to yours
+        </h2>
+      </div>
+
+      {/* Cards */}
+      <div className={styles.cardsContainer}>
+        <div className={styles.cardsContent}>
+          <div className={styles.cardsTitleContainer}>
+            <h2 className="title-large">Staff Picks</h2>
+            <Link href={`#`}>View selected products</Link>
+          </div>
+          <ProductCardList products={staffPicks} />
+        </div>
+      </div>
     </section>
   );
 }
